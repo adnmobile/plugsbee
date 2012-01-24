98d7a9fd7f7808aa2d134dece9831553a9770480
Sonny Piers <sonny.piers@gmail.com>
2012-01-24 15:42:29 +0100

    update websocket servers url

--- configuration.js
+++ configuration.js
@@ -3,12 +3,17 @@ var gConfiguration = {
 	uploadService : 'http://upload.plugsbee.com',
 	theme: 'default',
 };
-// If safari mobile
-if(navigator.userAgent.match('AppleWebKit') && !navigator.userAgent.match('Chrome') && navigator.userAgent.match('Mobile'))
-  gConfiguration.WebsocketService = "wsold://ws.plugsbee.com";
-// If safari desktop
-else if(navigator.userAgent.match('AppleWebKit') && !navigator.userAgent.match('Chrome') && !navigator.userAgent.match('Mobile'))
-  window.WEB_SOCKET_FORCE_FLASH = true;
+//~ // If safari mobile
+//~ if(navigator.userAgent.match('AppleWebKit') && !navigator.userAgent.match('Chrome') && navigator.userAgent.match('Mobile'))
+  //~ gConfiguration.WebsocketService = "ws://oldws.plugsbee.com";
+//~ // If safari desktop
+//~ else if(navigator.userAgent.match('AppleWebKit') && !navigator.userAgent.match('Chrome') && !navigator.userAgent.match('Mobile'))
+  //~ window.WEB_SOCKET_FORCE_FLASH = true;
+  
+//If safari
+if(navigator.userAgent.match('AppleWebKit') && !navigator.userAgent.match('Chrome'))
+  gConfiguration.WebsocketService = "ws://oldws.plugsbee.com";
+//Else
 else
   gConfiguration.WebsocketService = "ws://ws.plugsbee.com";
 
