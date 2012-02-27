0bd43a2284d780f0f39ee092c7fcf7515f7adac6
Sonny Piers <sonny.piers@gmail.com>
2012-02-27 15:05:19 +0100

    files changes

--- burger/burger.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var Widget = {
-  parser : new DOMParser(),
-  parse: function(aStr) {
-    return this.parser.parseFromString(aStr, "application/xhtml+xml").documentElement;
-  }
-};
