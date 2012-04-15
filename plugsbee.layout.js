ee1ea2ebf5d3368272807ec9ace677ac9d72cee0
Sonny Piers <sonny.piers@gmail.com>
2012-04-15 21:04:39 +0200

    makes scroll more agressive

--- plugsbee.layout.js
+++ plugsbee.layout.js
@@ -82,10 +82,10 @@ Plugsbee.layout = {
     panel.elm.setAttribute('data-name', aPbFolder.id);
     panel.elm.classList.add('hidden');
     panel.elm.addEventListener('mousewheel', function(e) {
-      this.scrollTop = this.scrollTop-Math.round((e.wheelDelta/60)*10);
+      this.scrollTop = this.scrollTop-Math.round((e.wheelDelta/60)*30);
     });
     panel.elm.addEventListener('DOMMouseScroll', function(e) {
-      this.scrollTop = this.scrollTop-Math.round((e.detail/2)*10);
+      this.scrollTop = this.scrollTop-Math.round((e.detail/2)*30);
     });
     aPbFolder.panel = panel;
   },
@@ -207,10 +207,10 @@ Plugsbee.layout = {
     (function() {
       var folders = document.querySelector('section[data-name="folders"]');
       folders.addEventListener('mousewheel', function(e) {
-        this.scrollTop = this.scrollTop-Math.round((e.wheelDelta/60)*60);
+        this.scrollTop = this.scrollTop-Math.round((e.wheelDelta/60)*30);
       });
       folders.addEventListener('DOMMouseScroll', function(e) {
-        this.scrollTop = this.scrollTop-Math.round((e.detail/2)*60);
+        this.scrollTop = this.scrollTop-Math.round((e.detail/2)*30);
       });
     })();
 
