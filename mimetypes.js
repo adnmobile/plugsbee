05d08991b7d365fc0e4d91690ba590f36305f6e2
Tho Chevalier <theo.chevalier11@gmail.com>
2012-04-20 00:57:39 +0200

    Mimetypes association

---
+++ mimetypes.js
@@ -0,0 +1,65 @@
+'use strict';
+(function() {
+Plugsbee.mimetyes = {
+  // PDF
+  'application/pdf': 'pdf',
+
+  // Text plain
+  'text/plain': 'text-plain'
+
+  // Richtext
+  'application/rtf': 'text-richtext',
+
+  // Wordprocessing
+  'application/msword': 'wordprocessing',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'wordprocessing',
+  'application/vnd.oasis.opendocument.text': 'wordprocessing',
+  'application/x-iwork-pages-sffpages': 'wordprocessing',
+
+  // Spreadsheet
+  'application/vnd.ms-excel': 'spreadsheet',
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'spreadsheet',
+  'application/vnd.oasis.opendocument.spreadsheet': 'spreadsheet',
+  'application/x-iwork-pages-sffnumbers': 'spreadsheet',
+
+  // Presentation
+  'application/vnd.ms-powerpoint': 'presentation',
+  'application/vnd.openxmlformats-officedocument.presentationml.presentation': 'presentation',
+  'application/vnd.oasis.opendocument.presentation': 'presentation',
+  'application/x-iwork-keynote-sffkey': 'presentation',
+
+  // Video
+  'video/webm': 'video',
+  'video/webm': 'video',
+  'video/mp4v-es': 'video',
+  'video/x-matroska': 'video',
+  'video/ogg': 'video',
+  'video/avi': 'video',
+  'video/quicktime': 'video',
+
+  // Audio
+  'audio/mpeg': 'audio',
+  'audio/ogg': 'audio',
+  'audio/flac': 'audio',
+  'audio/wav': 'audio',
+  'audio/mid': 'audio',
+
+  // Image
+  'image/png': 'image',
+  'image/gif': 'image',
+  'image/webp': 'image',
+  'image/jpeg': 'image',
+  'image/jpg': 'image',
+  'image/bmp': 'image',
+  'image/svg+xml': 'image',
+  'image/ico': 'image',
+  'image/photoshop': 'image',
+  'image/xcf': 'image',
+  'application/illustrator': 'image',
+
+  // Archive
+  'application/x-rar-compressed': 'archive',
+  'application/zip': 'archive',
+  'application/gzip': 'archive'
+};
+})();
